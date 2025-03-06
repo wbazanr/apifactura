6107ecfff0e673b8f098bfdf690c2145d0fe23a7
Wilson BAzan <wbazan@eglobalt.com.py>
2025-03-05 21:28:21 -0300

    update y delete fit

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -92,7 +92,36 @@ class ClienteController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $data = $request->validate([
+                'nombre' => 'required|string|max:50',
+                'apellido' => 'required|string',
+                'ruc' => 'required|string|max:100',
+                'email' => 'string|email|max:100',
+                'direccion' => 'nullable|string|max:255',
+                'razon_social' => 'nullable|string|max:255',
+                'fecha_nacimiento' => 'nullable||date|max:255',
+                'telefono' => 'nullable|string|max:255'
+    
+            ]);
+
+            $cliente = Cliente::findOrFail($id);
+
+            $cliente->update($data);
+
+            return response()->json(
+                                $this->get_response(    
+                                                    $this->respusta_exitosa,200,$cliente
+                                                    )
+
+                                );
+
+        }
+        catch (Exception $e) {
+
+            return $this->get_response($e->getMessage(),503,[]);
+
+        }
     }
 
     /**
@@ -100,6 +129,16 @@ class ClienteController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+    try {
+
+        $cliente = Cliente::findOrFail($id);
+        $cliente->delete();
+        return response()->json($this->get_response('Eiminado correctamente'.$id, 200,$cliente));
+
+    }catch (Exception $e) {
+        return $this->get_response($e->getMessage(),500,[]);
+    }
+
+
     }
 }
