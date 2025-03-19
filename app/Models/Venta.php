38943c7069b3e5d77b8fffd98e8a09f1d7ebe378
Wilson BAzan <wbazan@eglobalt.com.py>
2025-03-18 23:07:28 -0300

    Ventas,ventasdetalles

---
+++ app/Models/Venta.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Venta extends Model
+{
+    use HasFactory;
+    protected $fillable =[
+        'ciente_id',
+        'fecha_venta',
+        'total_venta',
+        'estado'
+    ];
+public function cliente() {
+
+    return $this->belongsTo(cliente::class);
+}
+public function productos()
+{
+    return $this->belongsToMany(productos::class,'ventas_detalles')
+    ->withPivot('cantidad','precio','subtotal');
+}
+public function detalles()
+{
+    return $this->hasMany(VentaDetalle::class);
+}
+
+
+}
