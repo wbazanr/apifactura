0e306320e2db492dcaf13b7564c833ec21c56f18
Wilson BAzan <wbazan@eglobalt.com.py>
2025-04-03 22:17:23 -0300

    ventas

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -16,7 +16,16 @@ class VentaController extends Controller
      */
     public function index()
     {
-        //
+        try{
+            $ventas = Venta::with('detalles.producto')->get();
+            return response()->json($this->get_response("listado de ventas",200,$ventas),200);
+
+
+        }
+
+        catch(Exception $e) {
+            return response()->json($this->get_response($e->getMessage(),500,null),500);
+        }
     }
 
     /**
@@ -99,7 +108,12 @@ class VentaController extends Controller
      */
     public function show(Venta $venta)
     {
-        //
+        try{
+            $respuesta = $venta->with('detalles.producto')->first();
+            return response()->json($this->get_response("Se recupero la venta",200,$respuesta),200);
+        }catch(Exception $e){
+            return response()->json($this->get_response($e->getMessage(),500,null),500);
+        }
     }
 
     /**
